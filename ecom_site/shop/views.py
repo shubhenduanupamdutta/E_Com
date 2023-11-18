b6614df30b1a385edad5fb8573326b219310a8db
ShubhenduADutta <shubhendu@flexli.in>
2023-11-18 23:06:28 +0530

    Modify view to add search functionality

--- ecom_site/shop/views.py
+++ ecom_site/shop/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import Product
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.db.models import Q
 # Create your views here.
+
 
 class Index(ListView):
     model = Product
     template_name = "shop/index.html"
     context_object_name = "products"
+    paginate_by = 10
+
+    def get_queryset(self):
+        # Retrieve the search parameter from the request
+        search_param = self.request.GET.get('item_name', '')
+
+        # Apply filtering based on the search parameter
+        query = Q(title__icontains=search_param)
+        query = query | Q(category__icontains=search_param)
+        query = query | Q(description__icontains=search_param)
+        queryset = Product.objects.filter(query)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Add the search parameter to the context for use in the template
+        context['search_param'] = self.request.GET.get('item_name', '')
+
+        return context
