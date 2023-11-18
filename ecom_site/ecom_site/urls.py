3eea8993542f4c5ccabc15dcd26562ea8123ffc9
ShubhenduADutta <shubhendu@flexli.in>
2023-11-18 21:34:25 +0530

    Register the index view to urlpattern

--- ecom_site/ecom_site/urls.py
+++ ecom_site/ecom_site/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop import views
 
 urlpatterns = [
+    path('', views.Index.as_view(), name="index"),
     path('admin/', admin.site.urls),
 ]
