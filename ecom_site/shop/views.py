099cbc3c8f1a2379fefb8091927ea38e1d260970
ShubhenduADutta <shubhendu@flexli.in>
2023-11-18 21:34:03 +0530

    Create view for home page, list view for products

--- ecom_site/shop/views.py
+++ ecom_site/shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Product
+from django.views.generic import ListView, DetailView
 # Create your views here.
+
+class Index(ListView):
+    model = Product
+    template_name = "shop/index.html"
+    context_object_name = "products"
