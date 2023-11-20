654662f2ab33bb92752e93543e46594ce5d5e3e4
ShubhenduADutta <shubhendu@flexli.in>
2023-11-20 13:59:16 +0530

    Show cart item count on navbar.

--- ecom_site/shop/static/shop/script.js
+++ ecom_site/shop/static/shop/script.js
@@ -5,16 +5,21 @@ if (localStorage.getItem("cart") == null) {
 } else {
   cart = JSON.parse(localStorage.getItem("cart"));
 }
-(document.querySelectorAll(".atc")).forEach((button) => {
+document.getElementById("cart").innerHTML = "Cart(" + Object.keys(cart).length + ")";
+
+document.querySelectorAll(".atc").forEach((button) => {
   button.addEventListener("click", (event) => {
-      var productId = event.target.id;
-      console.log(productId + " clicked");
-      if (cart[productId] != undefined) {
-        cart[productId] += 1;
-      } else {
-        cart[productId] = 1;
-      }
-      console.log(cart);
-      localStorage.setItem("cart", JSON.stringify(cart));
-    });
+    var productId = event.target.id;
+    console.log(productId + " clicked");
+    if (cart[productId] != undefined) {
+      cart[productId] += 1;
+    } else {
+      cart[productId] = 1;
+    }
+    console.log(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    console.log("cart updated");
+    document.getElementById("cart").innerHTML = "Cart(" + Object.keys(cart).length + ")";
+    // console.log(Object.keys(cart).length);
+  });
 });
