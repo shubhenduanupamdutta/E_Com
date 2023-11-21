428fa9d6a20b0219ca70b24abd105a23fc8e5985
ShubhenduADutta <shubhendu@flexli.in>
2023-11-21 19:31:56 +0530

    Add a created_at field.
    Added an created_at field to track when an order was placed.

--- ecom_site/shop/models.py
+++ ecom_site/shop/models.py
@@ -23,6 +23,7 @@ class Order(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} - {self.items}"
