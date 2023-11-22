2cceb9421c06e78391c7be9e7f8751a216fd72b1
ShubhenduADutta <shubhendu@flexli.in>
2023-11-22 12:39:34 +0530

    Add total to checkout list and style checkout page

--- ecom_site/shop/static/shop/script.js
+++ ecom_site/shop/static/shop/script.js
@@ -27,7 +27,6 @@ document.querySelectorAll(".atc").forEach((button) => {
     }
     console.log(cart);
     localStorage.setItem("cart", JSON.stringify(cart));
-    localStorage.setItem("item_name", JSON.stringify(item_name));
     console.log("cart updated");
     // console.log(productPrice, typeof productPrice);
     document.getElementById("cart").innerHTML = "Cart(" + Object.keys(cart).length + ")";
@@ -54,25 +53,35 @@ const popoverList = [...popoverTriggerList].map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl);
 });
 
+function create_checkout_list_item(productName, quantity, price) {
+  let item_str = `<span> <span class=""> ${quantity} </span> ${productName} </span>
+  <span class="badge bg-primary">&#8377; ${price}</span>`;
+  let list_item = document.createElement("li");
+  list_item.classList.add(
+    "list-group-item",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+  );
+
+  list_item.innerHTML = item_str;
+  document.getElementById("checkoutList").appendChild(list_item);
+}
+
+let total = 0;
 for (item in cart) {
   let productName = cart[item][1];
   let quantity = cart[item][0];
   let price = cart[item][2];
+  total += price;
   if (!(document.getElementById("checkoutList") == null)) {
-    let item_str = `${productName}
-  <span class="badge bg-primary rounded-pill">${quantity}</span>`;
-    let list_item = document.createElement("li");
-    list_item.classList.add(
-      "list-group-item",
-      "d-flex",
-      "justify-content-between",
-      "align-items-center"
-    );
-
-    list_item.innerHTML = item_str;
-    document.getElementById("checkoutList").appendChild(list_item);
+    create_checkout_list_item("of " + productName, quantity, price);
   }
 }
+if (Object.keys(cart).length > 0) {
+  create_checkout_list_item("Your Total Cart Value", "", total);
+}
+
 
 if (!(document.getElementById("items") == null)) {
   document.getElementById("items").value = JSON.stringify(cart);
