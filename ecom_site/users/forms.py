13b3221c41704b7acf3e119d7736ff2498f781ee
ShubhenduADutta <shubhendu@flexli.in>
2023-11-23 16:17:34 +0530

    Make email required and unique in forms.
    Made email during registration a required attribute and show error .

--- ecom_site/users/forms.py
+++ ecom_site/users/forms.py
@@ -9,3 +9,12 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email:
+            raise forms.ValidationError("Email is required")
+        # check if email is already registered
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists")
+        return email
