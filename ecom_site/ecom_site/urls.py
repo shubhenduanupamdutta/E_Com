2e5fcf76131c0a5f76ec814cf7f11522551b7b3b
ShubhenduADutta <shubhendu@flexli.in>
2023-11-23 13:58:17 +0530

    Add login view to urls

--- ecom_site/ecom_site/urls.py
+++ ecom_site/ecom_site/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
         name='product_detail'
     ),
     path('register/', user_views.register, name="register"),
-    path('login/', auth_views.LoginView.as_view(
+    path('login/', user_views.CustomLoginView.as_view(
         template_name="users/login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(
         template_name="users/logout.html"), name="logout"),
